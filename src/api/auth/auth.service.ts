b3b7df71de6e541723f9baa61c8cb4e92dc849e5
fatal-zxc <faaatal_l@proton.me>
2026-04-29 15:20:01 +0300

    feat: prisma

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -1,4 +1,7 @@
+import { SendOtpRequest } from '@fatal-cinema/contracts/gen/auth/v1/auth'
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	async sendOtp(data: SendOtpRequest) {}
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 
+import { CoreModule } from '@core/core.module'
 import { ApiModule } from '@api/api.module'
 
 @Module({
-	imports: [ApiModule],
+	imports: [CoreModule, ApiModule],
 })
 export class AppModule {}
