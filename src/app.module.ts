1c9c5673382514e608f6811e2228e597eed5b015
fatal-zxc <faaatal_l@proton.me>
2026-05-26 00:40:24 +0300

    feat: add observability metrics via prometheus

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common'
 
 import { CoreModule } from '@core/core.module'
 import { LibsModule } from '@libs/libs.module'
+import { ObservabilityModule } from '@observability/observability.module'
 import { SharedModule } from '@shared/shared.module'
 import { ApiModule } from '@api/api.module'
 
 @Module({
-	imports: [SharedModule, CoreModule, LibsModule, ApiModule],
+	imports: [SharedModule, CoreModule, LibsModule, ObservabilityModule, ApiModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
 	createGrpcServer(app, config)
 
 	await app.startAllMicroservices()
-	await app.init()
+	await app.listen(9101)
 }
 bootstrap()
